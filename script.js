34a25127fa9bf594da202f29faf450039220cdf7
ftmhabt <ftmh.a78@gmail.com>
2024-01-24 00:01:40 +0330

    Visualize the library

--- script.js
+++ script.js
@@ -25,7 +25,42 @@ function addBookToLibrary() {
     myLibrary.push(new Book(title,author,pages,isRead));
     console.table(myLibrary);
 
+    showLibrary();
+}
 
+function showLibrary(){
+  let list=document.querySelector('.list'); 
+  while(list.firstChild)  {
+    list.removeChild(list.firstChild);
+  }
+  
+
+  for (let i = 0; i < myLibrary.length; i++) {
+    let div= document.createElement('div');
+    div.classList.add('book');
+    let t= document.createElement('div');
+    let a= document.createElement('div');
+    let p= document.createElement('div');
+    let r= document.createElement('div');
+    
+    t.textContent=myLibrary[i].title;
+    a.textContent=myLibrary[i].author;
+    p.textContent=myLibrary[i].pages;
+    r.textContent=myLibrary[i].isRead;
+
+    let hr=document.createElement('hr');
+
+    list.appendChild(div);
+    list.appendChild(hr);
+
+    div.appendChild(t);
+    div.appendChild(a);
+    div.appendChild(p);
+    div.appendChild(r);
+    
+    
+    
+  }
 }
 
 // add book to library button
