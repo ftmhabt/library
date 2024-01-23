40fe4552f358f5ad2b80384de760c5eb3ef55ebc
ftmhabt <ftmh.a78@gmail.com>
2024-01-23 23:17:19 +0330

    Add form button script (functioning but not visualized)

--- script.js
+++ script.js
@@ -13,20 +13,28 @@ Book.prototype.info = function(){
     return `${this.title} by ${this.author}, ${this.pages} pages, ${this.isRead?"read":"not read yet"} `;
 }
 
-function addBookToLibrary() {
-    title=prompt("enter the title");
-    author=prompt("enter the author");
-    pages=prompt("enter the pages");
-    isRead=prompt("is read or not? y/n");
 
-    myLibrary.push(new Book(title,author,pages,isRead==='y'?true:false));
+function addBookToLibrary() {
+    title=document.querySelector('#title').value;
+    author=document.querySelector('#author').value;
+    pages=document.querySelector('#pages').value;
+    isRead=document.querySelector('#is-read').checked;
+
+    console.log(title,author,pages,isRead);
+
+    myLibrary.push(new Book(title,author,pages,isRead));
     console.table(myLibrary);
+
+
 }
 
-
-
-// const book1=new Book("Horizon","me",235,true);
-// console.log(book1.info());
+// add book to library button
+let addBtn=document.querySelector('#add');
+addBtn.addEventListener('click',function(event){
+  event.preventDefault();
+  addBookToLibrary();
+  form.reset();
+});
 
 
 
